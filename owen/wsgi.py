806f887b3b6d1bf7fb9aae236f3abcf1acfd51e3
Julia Ebert <julia@juliaebert.com>
2016-12-15 10:27:03 -0500

    Revert back to not using compressor stuff

--- owen/wsgi.py
+++ owen/wsgi.py
@@ -7,15 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-#from __future__ import absolute_import, unicode_literals
-#import os
-#from django.core.wsgi import get_wsgi_application
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "owen.settings.dev")
-#application = get_wsgi_application()
+from __future__ import absolute_import, unicode_literals
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "owen.settings.dev")
+application = get_wsgi_application()
 
 
 # FOR HEROKU
-
+'''
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
@@ -24,3 +24,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_title.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+'''
