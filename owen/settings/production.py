4a2e6a0249bd9bed96600cb74631ba44858206c9
Julia Ebert <julia@juliaebert.com>
2016-12-15 10:15:55 -0500

    Adding static file stuff

--- owen/settings/production.py
+++ owen/settings/production.py
@@ -28,3 +28,12 @@ import os
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
--- owen/wsgi.py
+++ owen/wsgi.py
@@ -7,12 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-from __future__ import absolute_import, unicode_literals
+#from __future__ import absolute_import, unicode_literals
+#import os
+#from django.core.wsgi import get_wsgi_application
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "owen.settings.dev")
+#application = get_wsgi_application()
+
+
+# FOR HEROKU
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "owen.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_title.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
