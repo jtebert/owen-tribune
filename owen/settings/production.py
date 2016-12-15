bdd2c4045995c9bf2ed08892ffef05cae2a845b0
Julia Ebert <julia@juliaebert.com>
2016-12-15 10:05:01 -0500

    Add production .env stuff to settings

--- owen/settings/production.py
+++ owen/settings/production.py
@@ -21,3 +21,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
+
+# FOR HEROKU
+
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
