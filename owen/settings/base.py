e058f8fc62cc86ae35311cd929db4e1e0f40982f
Julia Ebert <julia@juliaebert.com>
2016-12-15 12:40:33 -0500

    Trying to get static/media files hosted on S3 because heroku breaks things

--- owen/settings/base.py
+++ owen/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 from __future__ import absolute_import, unicode_literals
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,8 +25,9 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
+    'owen',
     'home',
-    #'search',
+    'search',
     'images',
     'blog',
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'modelcluster',
     'taggit',
     'compressor',
+    'storages',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -131,7 +132,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -148,3 +149,37 @@ BASE_URL = 'http://example.com'
 WAGTAIL_SITE_NAME = "The Owen Tribune"
 
 WAGTAILIMAGES_IMAGE_MODEL = 'images.CustomImage'
+
+
+## FOR S3
+
+env = os.environ.copy()
+
+AWS_ACCESS_KEY_ID = 'AKIAJJOUJRBH63F3RPHQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+#AWS_SECRET_ACCESS_KEY = env['AWS_SECRET_ACCESS_KEY']
+#AWS_ACCESS_KEY_ID = env['AWS_ACCESS_KEY_ID']
+AWS_STORAGE_BUCKET_NAME = 'owen-tribune'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+#STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'owen.custom_storages.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
