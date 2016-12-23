c79a3fc08d2c5f5ff7deadc89d2bdef4aaf4ade7
Julia Ebert <ebertjulia@gmail.com>
2016-12-23 14:05:09 -0600

    Files are being collected locally instead of to AWS. That's at least part of the problem. Trying commenting out compressor stuff in production settings

--- owen/settings/production.py
+++ owen/settings/production.py
@@ -29,14 +29,14 @@ import os
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
+#COMPRESS_OFFLINE = True
+#COMPRESS_CSS_FILTERS = [
+#    'compressor.filters.css_default.CssAbsoluteFilter',
+#    'compressor.filters.cssmin.CSSMinFilter',
+#]
+#COMPRESS_CSS_HASHING_METHOD = 'content'
 
 # FOR HEROKU AWS
 
